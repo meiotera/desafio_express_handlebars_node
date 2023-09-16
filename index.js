89b43b44809a59ee47c72919fef941febaa7efe1
Renan Ferreira <renanfc.re@gmail.com>
2023-09-16 13:24:11 -0300

    retirada pasta node

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path')
+const app = express();
+const port = process.env.PORT || 3000;
+const contatoRouter = require('./routes/contato');
+const fs = require('fs');
+const hbs = exphbs.create({
+    partialsDir: ['views/partials']
+});
+
+
+app.use(express.static('public'))
+app.engine('handlebars', hbs.engine);
+app.set('veiw engine', 'handlebars');
+
+app.use('/', contatoRouter);
+
+
+
+app.get('/', (req, res) => {
+    fs.readFile('./database/produtos.json', (err, data) => {
+        const dados = JSON.parse(data);
+        const oculos = dados.oculos;
+        let produtos = []
+
+        oculos.forEach((oculo, index) => {
+            produtos.push(
+                {
+                    modelo: oculo.modelo,
+                    marca: oculo.marca,
+                    cor: oculo.cor,
+                    material: oculo.material,
+                    preco: oculo.preco.toFixed(2),
+                    img: oculo.img
+                }
+            );
+        });
+
+        res.render('home.handlebars', { produtos });
+    });
+})
+
+
+
+app.listen(port)
---
+++ routes/contato.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router()
+const path = require('path');
+const caminho = path.join(__dirname, '../views');
+
+router.get('/contato', (req, res) => {
+
+    const links = ['Instagram', 'FaceBook', 'Github']
+
+    res.render(`${caminho}/contato.handlebars`, { links });
+})
+
+
+module.exports = router;
